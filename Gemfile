5ca2f3a2705d3353d2e47a16cc67293bb3924d06
Andrejs <andrejs.cunskis@gmail.com>
2022-10-15 14:13:18 +0300

    Add static rubocop checks (#12)
    * Add static rubocop checks
    
    * Allign arguments

--- Gemfile
+++ Gemfile
@@ -1,8 +1,10 @@
-source 'https://rubygems.org'
+# frozen_string_literal: true
+
+source "https://rubygems.org"
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # Specify your gem's dependencies in sidekiq_alive.gemspec
 gemspec
 
-gem 'pry'
+gem "pry"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,24 @@
-require 'bundler/setup'
-require 'sidekiq-alive-next'
-require 'rspec-sidekiq'
-require 'mock_redis'
-require 'rack/test'
-require 'pry'
+# frozen_string_literal: true
 
-ENV['RACK_ENV'] = 'test'
-ENV['HOSTNAME'] = 'test-hostname'
+require "bundler/setup"
+require "sidekiq-alive-next"
+require "rspec-sidekiq"
+require "mock_redis"
+require "rack/test"
+require "pry"
+
+ENV["RACK_ENV"] = "test"
+ENV["HOSTNAME"] = "test-hostname"
 # initialize server
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
+  config.example_status_persistence_file_path = ".rspec_status"
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
-  config.expect_with :rspec do |c|
+  config.expect_with(:rspec) do |c|
     c.syntax = :expect
   end
 

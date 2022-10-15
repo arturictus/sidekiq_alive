be88c86febe5bb2c4bc7e1f735452bb1f7c490e5
Andrejs <andrejs.cunskis@gmail.com>
2022-10-15 16:16:47 +0300

    Use scan for finding and purging jobs (#15)

--- lib/sidekiq-alive-next.rb
+++ lib/sidekiq-alive-next.rb
@@ -14,7 +14,7 @@ module SidekiqAlive
       Sidekiq.configure_server do |sq_config|
         sq_config.on(:startup) do
           SidekiqAlive::Worker.sidekiq_options(queue: current_queue)
-          (sq_config.respond_to?(:[]) ? sq_config[:queues] : sq_config.options[:queues]).unshift(current_queue)
+          (sidekiq_6 ? sq_config[:queues] : sq_config.options[:queues]).unshift(current_queue)
 
           logger.info(startup_info)
 
@@ -70,12 +70,17 @@ module SidekiqAlive
     end
 
     def purge_pending_jobs
-      # TODO: Sidekiq 6 allows better way to find scheduled jobs
-      # https://github.com/mperham/sidekiq/wiki/API#scan
       scheduled_set = Sidekiq::ScheduledSet.new
-      jobs = scheduled_set.select { |job| job.klass == "SidekiqAlive::Worker" && job.queue == current_queue }
+
+      jobs = if sidekiq_6
+        scheduled_set.scan('"class":"SidekiqAlive::Worker"')
+      else
+        scheduled_set.select { |job| job.klass == "SidekiqAlive::Worker" && job.queue == current_queue }
+      end
+
       logger.info("[SidekiqAlive] Purging #{jobs.count} pending for #{hostname}")
       jobs.each(&:delete)
+
       logger.info("[SidekiqAlive] Removing queue #{current_queue}")
       Sidekiq::Queue.new(current_queue).clear
     end
@@ -144,7 +149,13 @@ module SidekiqAlive
     def register_instance(instance_name)
       redis.set(instance_name, Time.now.to_i, ex: config.registration_ttl.to_i)
     end
-end
+
+    private
+
+    def sidekiq_6
+      Sidekiq.respond_to?(:[])
+    end
+  end
 end
 
 require "sidekiq_alive/worker"

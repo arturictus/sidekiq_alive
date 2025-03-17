350e764e9da746f621b1a237557cc2e74559b48a
Kevin O'Connor <90646132+koconnor-ampion@users.noreply.github.com>
2025-03-17 08:48:14 -0400

    Update worker.rb
    Remove BAP-1823 changes like I told you to, Github!

--- lib/sidekiq_alive/worker.rb
+++ lib/sidekiq_alive/worker.rb
@@ -1,28 +1,18 @@
 # frozen_string_literal: true
 
-require "timeout"
-
 module SidekiqAlive
   class Worker
     include Sidekiq::Worker
-    sidekiq_options retry: true
-
-    TIMEOUT = 30
+    sidekiq_options retry: false
 
     # Passing the hostname argument it's only for debugging enqueued jobs
     def perform(_hostname = SidekiqAlive.hostname)
-      Timeout.timeout(TIMEOUT) do
-        # Checks if custom liveness probe passes should fail or return false
-        return unless config.custom_liveness_probe.call
+      return unless config.custom_liveness_probe.call
 
-        # Writes the liveness in Redis
-        write_living_probe
-        # schedules next living probe
-        self.class.perform_in(config.time_to_live / 2, current_hostname)
-      end
-    rescue => e
-      Rails.logger.error("SidekiqAlive: #{e}")
-      raise e
+      # Writes the liveness in Redis
+      write_living_probe
+      # schedules next living probe
+      self.class.perform_in(config.time_to_live / 2, current_hostname)
     end
 
     def write_living_probe

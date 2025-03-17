143fb2156ca28c7862d354cbd6b947d0d4822414
Kevin O'Connor <90646132+koconnor-ampion@users.noreply.github.com>
2025-03-17 09:00:42 -0400

    Update worker.rb
    unintended change

--- lib/sidekiq_alive/worker.rb
+++ lib/sidekiq_alive/worker.rb
@@ -7,6 +7,7 @@ module SidekiqAlive
 
     # Passing the hostname argument it's only for debugging enqueued jobs
     def perform(_hostname = SidekiqAlive.hostname)
+      # Checks if custom liveness probe passes should fail or return false
       return unless config.custom_liveness_probe.call
 
       # Writes the liveness in Redis
